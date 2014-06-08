2e20f8f48d62615fd00f6969e1c0b6beca544971
arcoard <arcoard+github@gmail.com>
2014-06-08 14:13:36 -0700

    Switched boilerplate + added docs.  Code now works to print basic metrics.  Working on documenting.

--- analytics.py
+++ analytics.py
@@ -1,107 +1,166 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This code has been has been written by Adam Coard, but was originally based 
+# off of the Google Analytics 'Hello' tutorial API.  So, credit where it is due, 
+# none of this is possible without Google's extensive API and documentation.
 
-"""Command-line skeleton application for Google Analytics API.
-Usage:
+
+"""
+ToDo:
+
+-Setup multiple account/site management (refactor 'get_first_profile_id')
+-Build output system that renders utilizing HTML templates
+-Have a discussion with Gary about what analytics/metrics he thinks are most important.
+
+Sample Usage:
+
   $ python analytics.py
-
-You can also get help on all the command-line flags the program understands
-by running:
-
-  $ python analytics.py --help
 
 """
 
+__author__ = 'arcoard@gmail.com (Adam Coard)'
+
 import argparse
-import httplib2
-import os
 import sys
+import pdb
 
-from apiclient import discovery
-from oauth2client import file
-from oauth2client import client
-from oauth2client import tools
-
-# Parser for command-line arguments.
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[tools.argparser])
-
-
-# CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
-# application, including client_id and client_secret. You can see the Client ID
-# and Client secret on the APIs page in the Cloud Console:
-# <https://cloud.google.com/console#/project/814529775042/apiui>
-CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
-
-# Set up a Flow object to be used for authentication.
-# Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
-# NEED. For more information on using scopes please see
-# <https://developers.google.com/+/best-practices>.
-FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
-  scope=[
-      'https://www.googleapis.com/auth/analytics',
-      'https://www.googleapis.com/auth/analytics.edit',
-      'https://www.googleapis.com/auth/analytics.manage.users',
-      'https://www.googleapis.com/auth/analytics.provision',
-      'https://www.googleapis.com/auth/analytics.readonly',
-    ],
-    message=tools.message_if_missing(CLIENT_SECRETS))
+from apiclient.errors import HttpError
+from apiclient import sample_tools
+from oauth2client.client import AccessTokenRefreshError
 
 
 def main(argv):
-  # Parse the command-line flags.
-  flags = parser.parse_args(argv[1:])
+  # Authenticate and construct service.
+  service, flags = sample_tools.init(
+      argv, 'analytics', 'v3', __doc__, __file__,
+      scope='https://www.googleapis.com/auth/analytics.readonly')
 
-  # If the credentials don't exist or are invalid run through the native client
-  # flow. The Storage object will ensure that if successful the good
-  # credentials will get written back to the file.
-  storage = file.Storage('sample.dat')
-  credentials = storage.get()
-  if credentials is None or credentials.invalid:
-    credentials = tools.run_flow(FLOW, storage, flags)
-
-  # Create an httplib2.Http object to handle our HTTP requests and authorize it
-  # with our good Credentials.
-  http = httplib2.Http()
-  http = credentials.authorize(http)
-
-  # Construct the service object for the interacting with the Google Analytics API.
-  service = discovery.build('analytics', 'v3', http=http)
-
+  # Try to make a request to the API. Print the results or handle errors.
   try:
-    print "Success! Now add code here."
+    first_profile_id = get_first_profile_id(service)
+    if not first_profile_id:
+      print 'Could not find a valid profile for this user.'
+    else:
+      # results = get_top_keywords(service, first_profile_id)
+      results = get_info(service, first_profile_id)
+      print_results(results)
+      pdb.set_trace()
 
-  except client.AccessTokenRefreshError:
-    print ("The credentials have been revoked or expired, please re-run"
-      "the application to re-authorize")
+  except TypeError, error:
+    # Handle errors in constructing a query.
+    print ('There was an error in constructing your query : %s' % error)
+
+  except HttpError, error:
+    # Handle API errors.
+    print ('Arg, there was an API error : %s : %s' %
+           (error.resp.status, error._get_reason()))
+
+  except AccessTokenRefreshError:
+    # Handle Auth errors.
+    print ('The credentials have been revoked or expired, please re-run '
+           'the application to re-authorize')
 
 
-# For more information on the Google Analytics API you can visit:
-#
-#   https://developers.google.com/analytics/
-#
-# For more information on the Google Analytics API Python library surface you
-# can visit:
-#
-#   https://developers.google.com/resources/api-libraries/documentation/analytics/v3/python/latest/
-#
-# For information on the Python Client Library visit:
-#
-#   https://developers.google.com/api-client-library/python/start/get_started
+def get_first_profile_id(service):
+  """Traverses Management API to return the first profile id.
+
+  This first queries the Accounts collection to get the first account ID.
+  This ID is used to query the Webproperties collection to retrieve the first
+  webproperty ID. And both account and webproperty IDs are used to query the
+  Profile collection to get the first profile id.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+
+  Returns:
+    A string with the first profile ID. None if a user does not have any
+    accounts, webproperties, or profiles.
+  """
+
+  accounts = service.management().accounts().list().execute()
+
+  if accounts.get('items'):
+    firstAccountId = accounts.get('items')[0].get('id')
+    webproperties = service.management().webproperties().list(
+        accountId=firstAccountId).execute()
+
+    if webproperties.get('items'):
+      firstWebpropertyId = webproperties.get('items')[0].get('id')
+      profiles = service.management().profiles().list(
+          accountId=firstAccountId,
+          webPropertyId=firstWebpropertyId).execute()
+
+      if profiles.get('items'):
+        return profiles.get('items')[0].get('id')
+
+  return None
+
+def get_top_keywords(service, profile_id):
+  """Executes and returns data from the Core Reporting API.
+
+  This queries the API for the top 25 organic search terms by visits.
+
+  Args:
+    service: The service object built by the Google API Python client library.
+    profile_id: String The profile ID from which to retrieve analytics data.
+
+  Returns:
+    The response returned from the Core Reporting API.
+  """
+
+  return service.data().ga().get(
+      ids='ga:' + profile_id,
+      start_date='2014-04-01',
+      end_date='2014-06-15',
+      metrics='ga:visits',
+      dimensions='ga:source,ga:keyword',
+      sort='-ga:visits',
+      filters='ga:medium==organic',
+      start_index='1',
+      max_results='25').execute()
+
+def get_info(service, profile_id):
+  return service.data().ga().get(
+    ids='ga:' + profile_id,
+    start_date='2012-01-01',
+    end_date='2014-06-15',
+    dimensions='ga:userType',
+    metrics='ga:sessions, ga:sessionDuration',
+    start_index='1',
+    max_results='25').execute()
+
+def print_results(results):
+  """Prints out the results.
+
+  This prints out the profile name, the column headers, and all the rows of
+  data.
+
+  Args:
+    results: The response returned from the Core Reporting API.
+  """
+
+  print
+  print 'Profile Name: %s' % results.get('profileInfo').get('profileName')
+  print
+
+  # Print header.
+  output = []
+  for header in results.get('columnHeaders'):
+    output.append('%30s' % header.get('name'))
+  print ''.join(output)
+
+  # Print data table.
+  if results.get('rows', []):
+    for row in results.get('rows'):
+      output = []
+      for cell in row:
+        output.append('%30s' % cell)
+      print ''.join(output)
+
+  else:
+    print 'No Rows Found'
+
+
 if __name__ == '__main__':
   main(sys.argv)
