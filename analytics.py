a727d3e09a96b5105aa03dcfbcb01634557f77c6
arcoard <arcoard+github@gmail.com>
2014-06-18 12:44:26 -0700

    .

--- analytics.py
+++ analytics.py
@@ -14,7 +14,10 @@ Todo:
 ->Extend functionality to link site to email address.
 -->Note: The email returned by the Google API is for the account (i.e. all are arcoard@gmail.com)
 -->How does Gary link email to client?  DB or file.py that is list of tuples with (email, websiteUrl)
-->Separate ContentPresentor into a separate file.
+
+->Build server backend:
+-->Ability to unsubscribe
+-->Send out weekly emails.
 
 ANALYTICS TO IMPLEMENT:
   Top Locations
@@ -420,78 +423,6 @@ class AnalyticsWrapper:
     else:
       print 'No Rows Found'
   
-
-# class ContentPresentor:
-#   """
-#   Takes the result from the AnalyticsWrapper.organize_results(), and readies 
-#   the data for presentation.  Output methods will include:
-
-#     1. Render via HTML template to create an HTML file.
-#     2. Email
-#   """
-
-#   def __init__(self, content, site_name):
-#     """
-#     Content must be a list of 
-#     """
-#     if isinstance(content, list):
-#       self.content = content  
-#     else:
-#       raise Exception("Error in instantiating ContentPresentor: Input must be a list.")
-
-#     self.site_name = site_name
-
-#   def run(self):
-#     self.render_template(self.content, self.site_name)
-
-#   def render_template(self, content, site_name):
-#     '''
-#     Renders HTML template with data.
-#     '''
-#     template_data = content
-#     templateLoader = jinja2.FileSystemLoader( searchpath="./" )
-#     templateEnv = jinja2.Environment( loader=templateLoader )
-#     TEMPLATE_FILE = "default.template"
-#     template = templateEnv.get_template( TEMPLATE_FILE )
-#     templateVars = { "title" : "Outfirst Insights",
-#               "site_name": site_name,
-#               "tables" : template_data,
-#            }
-
-#     outputText = template.render( templateVars )
-#     self.write_to_file(outputText, 'temp.html')
-    
-#     return outputText
-
-#   def email(self, rendered_template):
-#     """This function currently doesn't work because I don't have the SMTP info.
-#     """
-#     sending_email = "outfirst@email.com"
-#     recepient = "your@email.com"
-
-#     msg = MIMEMultipart('alternative')
-#     msg['Subject'] = "Outfirst Insights" #TODO: 'for the week of XXX'
-#     msg['From'] = sending_email
-#     msg['To'] = recepient
-
-#     content = MIMEText(html, rendered_template)
-#     msg.attach(content)
-#     # Send the message via local SMTP server.
-#     s = smtplib.SMTP('localhost') #TODO: Fill this out.
-#     s.sendmail(sending_email, recipient, msg.as_string())
-#     s.quit()
-
-
-#   def write_to_file(self, content, output_file_name):
-#     """
-#     Simplifies creating/writing to files.  Used for template rendering but generic enough for other purposes.
-#     """
-#     try:
-#       output = open(output_file_name, 'w')
-#       output.write(content)
-#       output.close()
-#     except Error, e:
-#       print "Write to file exception: " + e
 
 if __name__ == '__main__':
   api = AnalyticsWrapper()
